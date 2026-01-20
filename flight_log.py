94e61221191d10917144e4a56b36f18c1613f76b
Paul Bogard <paulbogard@gmail.com>
2026-01-19 21:16:22 -0500

    Delegate argparse commands to functions

--- flight_log.py
+++ flight_log.py
@@ -2,11 +2,19 @@
 
 import argparse
 
+def import_boarding_passes():
+    """Imports digital boarding passes."""
+    print("Importing digital boarding passes...")
+
+def import_recent():
+    """Finds recent flights on Flight Historian API and imports them."""
+    print("Importing Flight Historian recent flights...")
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Tools for interacting with a local flight log."
     )
-    subparsers = parser.add_subparsers()
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
     # import-boarding-passes
     parser_import_boarding_passes = subparsers.add_parser(
@@ -22,4 +30,10 @@ if __name__ == "__main__":
 
     # Parse arguments
     args = parser.parse_args()
-    print(args)
+    match args.command:
+        case "import-boarding-passes":
+            import_boarding_passes()
+        case "import-recent":
+            import_recent()
+        case _:
+            print("No command provided. See --help for commands.")

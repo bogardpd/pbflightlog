6f6f4a23961f6b059ac07601baf54d34580634cf
Paul Bogard <paulbogard@gmail.com>
2026-01-19 21:31:20 -0500

    Check for Flight Historian API key

--- flight_log.py
+++ flight_log.py
@@ -1,6 +1,11 @@
 """Tools for interacting with a local GeoPackage flight log."""
 
 import argparse
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file.
+load_dotenv()
 
 def import_boarding_passes():
     """Imports digital boarding passes."""
@@ -9,6 +14,12 @@ def import_boarding_passes():
 def import_recent():
     """Finds recent flights on Flight Historian API and imports them."""
     print("Importing Flight Historian recent flights...")
+    api_key_fh = os.getenv("API_KEY_FLIGHT_HISTORIAN")
+    if api_key_fh is None:
+        raise KeyError(
+            "Environment variable API_KEY_FLIGHT_HISTORIAN is missing."
+        )
+    print("Flight Historian API key is set.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
